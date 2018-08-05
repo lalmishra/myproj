d02a0e5bb7b901a898d6e698a910041a97d09e95
lal <lal.mishra14@gmail.com>
2018-08-05 11:54:21 +0530

    my code

---
+++ src/main/java/NIIT/OD/App.java
@@ -0,0 +1,13 @@
+package NIIT.OD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
